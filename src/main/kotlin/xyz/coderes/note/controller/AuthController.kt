37da66a123aeff853ba55cb0b8bb82bbc1ce8844
Vladimir <gold14@gmail.com>
2025-04-06 21:30:08 +0200

    Clean import

--- src/main/kotlin/xyz/coderes/note/controller/AuthController.kt
+++ src/main/kotlin/xyz/coderes/note/controller/AuthController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import xyz.coderes.note.database.security.AuthService
-import xyz.coderes.note.database.security.AuthService.*
 
 @RestController
 @RequestMapping("/auth")
